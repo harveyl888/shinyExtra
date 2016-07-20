41aa58598d02481b56864ec3b7e0c2af3b83134b
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-07-20 10:00:30 -0400

    inputId typo

--- R/SXPanel.R
+++ R/SXPanel.R
@@ -1,8 +1,8 @@
 ## SXPanel
 ##
-SXPanel <- function(inputID, ..., heading = '') {
-  pan.header <- shiny::div(class = 'panel-heading clearfix', id = paste0('panelheading_', inputID), heading)
-  pan.body <- shiny::div(class = 'panel-body', id = paste0('panelbody_', inputID), ...)
+SXPanel <- function(inputId, ..., heading = '') {
+  pan.header <- shiny::div(class = 'panel-heading clearfix', id = paste0('panelheading_', inputId), heading)
+  pan.body <- shiny::div(class = 'panel-body', id = paste0('panelbody_', inputId), ...)
   sx_panel <- shiny::div(class = 'panel panel-default', id = inputId, pan.header, pan.body)
   sx_panel
 }
