a4738191140df7fe2acefeed7ae1f06a812fe8d5
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-07-20 10:41:06 -0400

    Playing around with text sizes

--- R/SXPanel.R
+++ R/SXPanel.R
@@ -1,6 +1,6 @@
 ## SXPanel
 ##
-SXPanel <- function(inputId, heading = '', styleclass = 'default', icon = NULL, ...) {
+SXPanel <- function(inputId, heading = '', text_size = NULL, styleclass = 'default', icon = NULL, ...) {
 
   ## add a style
   if (styleclass %in% c('primary', 'info', 'success', 'warning', 'danger', 'inverse', 'link')) {
@@ -16,7 +16,20 @@ SXPanel <- function(inputId, heading = '', styleclass = 'default', icon = NULL, 
     pan_icon <- NULL
   }
 
-  pan_header <- shiny::div(class = 'panel-heading clearfix', id = paste0('panelheading_', inputId), heading, pan_icon)
+  ## adjust text size
+  if (is.null(text_size)) {
+    pan_textsize <- 'font-size: 100%;'
+  } else {
+    if (text_size == 'small') {
+      pan_textsize <- 'font-size: 75%;'
+    } else if (text_size == 'large') {
+      pan_textsize <- 'font-size: 150%;'
+    } else {
+      pan_textsize <- 'font-size: 100%;'
+    }
+  }
+
+  pan_header <- shiny::div(class = 'panel-heading clearfix', id = paste0('panelheading_', inputId), heading, style = pan_textsize, pan_icon)
   pan_body <- shiny::div(class = 'panel-body', id = paste0('panelbody_', inputId), ...)
   sx_panel <- shiny::div(class = pan_class, id = inputId, pan_header, pan_body)
   sx_panel
