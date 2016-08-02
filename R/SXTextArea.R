7f9c4650e9d0d05f17bb98797085296e01d9ed3a
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-08-02 14:53:37 -0400

    Added textarea

---
+++ R/SXTextArea.R
@@ -0,0 +1,32 @@
+#' SXTextArea
+#'
+#' helper function to include textarea in shiny apps
+#'
+#' @param inputId The input slot that will be used to access the value.
+#' @param label Display label for the control, or NULL for no label.
+#' @param text Text present in the textarea.
+#' @param placeholder Placeholder text for the textarea.
+#' @param resizable \bold{logical} - Allow textarea to be resized (default = true).
+#' @param rows number of rows (defaults to 5).
+#' @param cols number of columns (defaults to 40px).  100% covers the width of the parent.
+#'
+#' @export
+SXTextArea <- function(inputId, label = NULL, text = NULL, placeholder = NULL, resizable = TRUE, rows = 5, cols = 40) {
+
+  if (resizable == TRUE) {
+    textarea_class <- 'form-group shiny-input-container'
+  } else {
+    textarea_class <- 'form-group shiny-input-container textarea-noresize'
+  }
+
+  textarea_style <- 'max-width: 100%; max-height: 100%'
+
+  sx_textarea <-   tagList(tags$div(tags$strong(label), style="margin-top: 5px;"),
+                           tags$textarea(id = inputId,
+                                         class = textarea_class,
+                                         style = textarea_style,
+                                         placeholder = placeholder,
+                                         rows = rows,
+                                         cols = shiny::validateCssUnit(cols), text))
+  htmltools::attachDependencies(sx_textarea, shinySXDep)
+}
--- inst/examples/example01/app.R
+++ inst/examples/example01/app.R
@@ -3,6 +3,8 @@ library(shinyExtra)
 
 server <- function(input, output) {
 
+#  output$txt1_out <- renderText({input$txt1})
+
 }
 
 ui <- fluidPage(
@@ -10,14 +12,16 @@ ui <- fluidPage(
   fluidRow(
     column(4, SXPanel('pan1', heading = 'Regular header', checkbox = TRUE, styleclass = 'primary', icon = 'record',
                       h4('here is some text in the body'),
-                      SXLabel('lab1', 'This is a label', styleclass = 'success'),
+                      SXLabel('lab1', NULL, styleclass = 'success'),
                       SXLabel('lab1', 'This is a clickable label', styleclass = 'success', link='http://www.google.com')
     )),
     column(4, SXPanel('pan2', heading = 'Larger header', checkbox = TRUE, text_size = 'large', styleclass = 'success', collapsible = T, icon = 'ok',
                       h3('some text'),
+                      SXTextArea('txt1', 'Text Area Input', placeholder = 'enter some text', resizable = FALSE),
                       actionButton('but1', 'A BUTTON'))),
     column(4, SXPanel('pan3', collapsible = T, checkbox = T, heading = 'This is a very long header which will take up quite a bit of room, maybe too much!', text_size = 'small', styleclass = 'danger', icon = 'ok'))
   )
+#  verbatimTextOutput('txt1_out')
 )
 
 shinyApp(server = server, ui = ui)
