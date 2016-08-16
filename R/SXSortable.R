a0b7caeb375e5aee31172658b35b40c1a450868a
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-08-16 14:22:16 -0400

    Added titles above each list

--- R/SXSortable.R
+++ R/SXSortable.R
@@ -5,15 +5,22 @@
 #' @param inputId The input slot that will be used to access the data.
 #' @param labels A list of lists of labels.
 #' @param styleclass A list of Bootstrap styles to apply to the list. (\code{default, primary, success, info, warning, or danger}).
+#' @param headers A list of column headers.
 #' @param height A value for the height (default = 120 px)
 #' @param width A value for the width (default = maximum label width + 40 px)
 #'
 #' @export
-SXSortable <- function(inputId, labels = NULL, styleclass = NULL, height = NULL, width = NULL) {
+SXSortable <- function(inputId, labels = NULL, styleclass = NULL, headers = NULL, height = NULL, width = NULL) {
   if (is.null(labels)) return()
+
   styleclass <- as.list(styleclass)
   if (length(styleclass) < length(labels)) {
     styleclass <- c(styleclass, rep(length(labels) - length(styleclass), 'default'))
+  }
+
+  headers <- as.list(headers)
+  if (length(headers) < length(labels)) {
+    headers <- c(headers, rep(length(labels) - length(headers), 'no title'))
   }
 
   if (is.null(height)) {
@@ -23,13 +30,18 @@ SXSortable <- function(inputId, labels = NULL, styleclass = NULL, height = NULL,
   }
 
   l.labels <- list()
-  boxstyle <- paste0('display:inline-block; vertical-align:top; height:', height, ';')
+  boxstyle <- paste0('height:', height, ';')
   for (i in 1:length(labels)) {
     l1 <- list()
     for (j in 1:length(labels[[i]])) {
       l1[[j]] <- shiny::tags$span(SXLabel(paste0(inputId, '_label_', i, '_', j), label = labels[[i]][[j]], styleclass = styleclass[[i]]), class = 'sort_label')
     }
-    l.labels[[i]] <- shiny::div(id = paste0(inputId, '_', i), class = paste0('connectedSortable_', inputId, ' sortGroup'), shiny::tagList(l1), style = boxstyle)
+    l.labels[[i]] <- shiny::div(id = paste0(inputId, '_', i), class = 'sortGroupOuter', style = 'display:inline-block; vertical-align:top;',
+                                shiny::div(id = paste0(inputId, '_', i, '_title'), shiny::span(headers[[i]]), class = 'overflowText'),
+      shiny::div(
+        id = paste0(inputId, '_', i, '_labels'), class = paste0('connectedSortable_', inputId, ' sortGroup'), shiny::tagList(l1), style = boxstyle
+      )
+    )
   }
 
 
@@ -38,7 +50,7 @@ SXSortable <- function(inputId, labels = NULL, styleclass = NULL, height = NULL,
   sortable_js <- paste0('
     // initialize shiny variable containing values
     $(document).on("shiny:connected", function(event) {
-    ')
+  ')
   for (i in 1:length(labels)) {
     sortable_js <- paste0(sortable_js, '
       Shiny.onInputChange(\"', inputId, '_val_', i, '\" , [', paste0("\"", labels[[i]], "\"", collapse = ","), ']);
@@ -47,7 +59,7 @@ SXSortable <- function(inputId, labels = NULL, styleclass = NULL, height = NULL,
   sortable_js <- paste0(sortable_js, '
     });
     // update shiny variable when sortable list is changed
-    $(\"', paste0(paste0("#", inputId, "_", seq_along(labels)), collapse = ', '), '\").sortable({
+    $(\"', paste0(paste0("#", inputId, "_", seq_along(labels), "_labels"), collapse = ', '), '\").sortable({
       connectWith:".connectedSortable_', inputId, '",
       create: function( event, ui) {
         var setWidth = ', ifelse(is.null(width), "null", paste0("\"", shiny::validateCssUnit(width), "\"")), ';
@@ -55,9 +67,11 @@ SXSortable <- function(inputId, labels = NULL, styleclass = NULL, height = NULL,
           var widths = [];
           $(\"#', inputId, ' .sort_label .label").each(function() { widths.push($(this).outerWidth(true)) });
           maxWidth = Math.max.apply(Math, widths);
-          $(\"#', inputId, ' .sortGroup\").width(maxWidth + 40);
+          $(\"#', inputId, ' .sortGroupOuter\").width(maxWidth + 40);
+          $(\"#', inputId, ' .overflowText\").width(maxWidth + 40);
         } else {
-          $(\"#', inputId, ' .sortGroup\").width(setWidth);
+          $(\"#', inputId, ' .sortGroupOuter\").width(setWidth);
+          $(\"#', inputId, ' .overflowText\").width(setWidth);
         }
       },
       update: function( event, ui) {
@@ -65,7 +79,7 @@ SXSortable <- function(inputId, labels = NULL, styleclass = NULL, height = NULL,
         var counter = 0;
         $(\"#', inputId, ' .sortGroup\").each(function(){
           output = [];
-          counter ++;
+         counter ++;
           $(this).find(".label").each(function(){
             output.push($(this).text());
           })
@@ -73,7 +87,8 @@ SXSortable <- function(inputId, labels = NULL, styleclass = NULL, height = NULL,
         });
       }
     })
-                        ')
+  ')
+
   sortable_js <- shiny::tags$script(sortable_js)
 
   sx_sortable <- shiny::tagList(shiny::div(id = inputId, shiny::tagList(l.labels)), sortable_js)
