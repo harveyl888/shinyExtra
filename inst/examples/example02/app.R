2f365f363519dbdae5e732851e8b1f3513149c22
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-08-23 10:11:18 -0400

    Hack to fix renderUI issue - fixes #4

---
+++ inst/examples/example02/app.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(shinyExtra)
+
+## Test example to ensure that initial values are set when envoking widget within renderUI
+
+
+server <- function(input, output, session) {
+
+  output$txt1 <- renderPrint({input$sort1_val_2})
+  output$txt2 <- renderPrint({input$sort2_val_2})
+
+  session$onFlushed(function() {
+    session$sendCustomMessage(type = "initSort", 1)
+  }, once=TRUE)
+
+  output$uisort2 <- renderUI({
+    SXSortable('sort2', labels = list(list("label 1", "label 2", "label 3"), list("label 4", "label 5")), styleclass = list("success", "primary"), headers=list("title 1", "title 2"), colorByGroup = TRUE, height = 100)
+  })
+
+}
+
+ui <- fluidPage(
+  br(),
+  column(4,
+         SXSortable('sort1', labels = list(list("label 1", "label 2"), list("label 3", "label 4", "label 5")), styleclass = list("success", "primary"), headers=list("title 1", "title 2"), colorByGroup = TRUE, height = 100),
+         verbatimTextOutput('txt1')
+  ),
+  column(4,
+         uiOutput('uisort2'),
+         verbatimTextOutput('txt2')
+  )
+)
+
+shinyApp(server = server, ui = ui)
