7fb91e767028c40ac742ee27747657383da6ed96
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-09-06 12:00:12 -0400

    bugfix for empty list in SXsortable

--- inst/examples/example01/app.R
+++ inst/examples/example01/app.R
@@ -22,8 +22,9 @@ ui <- fluidPage(
                       actionButton('but1', 'A BUTTON'))),
     column(4, SXPanel('pan3', collapsible = T, checkbox = T, heading = 'This is a very long header which will take up quite a bit of room, maybe too much!', text_size = 'small', styleclass = 'danger', icon = 'ok',
 #                      SXSortable('sort1', labels = list(list('label1', 'label2', 'label3'), list('label4', 'label5', 'label6'), list('X1', 'X2', 'X3', 'X4')), styleclass = list('success', 'primary', 'warning'), height = 100)
-SXSortable('sort1', labels = list(list("label 1", "label 2", "label 3"), list("label 4", "label 5")), styleclass = list("success", "primary"), headers=list("title 1", "title 2"), colorByGroup = TRUE, height = 100)
-                      ))
+#SXSortable('sort1', labels = list(list("label 1", "label 2", "label 3"), list("label 4", "label 5")), styleclass = list("success", "primary"), headers=list("title 1", "title 2"), colorByGroup = TRUE, height = 100)
+SXSortable('sort1', labels = list(list("label 1", "label 2", "label 3"), list()), styleclass = list("success", "primary"), headers=list("title 1", "title 2"), colorByGroup = TRUE, height = 100)
+    ))
   ),
   verbatimTextOutput('txt1')
 )
